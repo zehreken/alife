523b149f7bcce423902540a51a1e820763786221
zehreken <zehreken@gmail.com>
2019-12-04 23:43:24 +0100

    Create first window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
-fn main() {
-    println!("Hello, world!");
+use amethyst::{
+    prelude::*,
+    renderer::{
+        plugins::{RenderFlat2D, RenderToWindow},
+        types::DefaultBackend,
+        RenderingBundle,
+    },
+    utils::application_root_dir,
+};
+
+pub struct Pong;
+impl SimpleState for Pong {}
+
+fn main() -> amethyst::Result<()> {
+    // We'll put the rest of the code here.
+    amethyst::start_logger(Default::default());
+
+    let app_root = application_root_dir()?;
+    let display_config_path = app_root.join("config").join("display.ron");
+
+    let game_data = GameDataBuilder::default().with_bundle(
+        RenderingBundle::<DefaultBackend>::new()
+            // The RenderToWindow plugin provides all the scaffolding for opening a window and drawing on it
+            .with_plugin(
+                RenderToWindow::from_config_path(display_config_path)
+                    .with_clear([0.0, 0.0, 0.0, 1.0]),
+            )
+            // RenderFlat2D plugin is used to render entities with a `SpriteRender` component.
+            .with_plugin(RenderFlat2D::default()),
+    )?;
+
+    let assets_dir = app_root.join("assets");
+    let mut world = World::new();
+    let mut game = Application::new(assets_dir, Pong, game_data)?;
+    game.run();
+
+    std::thread::sleep(std::time::Duration::from_millis(20));
+
+    Ok(())
 }
