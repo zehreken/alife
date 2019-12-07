59cae76d18d2978443bca05e527974524f735ab9
zehreken <zehreken@gmail.com>
2019-12-07 21:31:35 +0100

    Fill ui with score data

--- src/systems/winner.rs
+++ src/systems/winner.rs
@@ -2,26 +2,42 @@ use amethyst::{
     core::transform::Transform,
     core::SystemDesc,
     derive::SystemDesc,
-    ecs::prelude::{Join, System, SystemData, World, WriteStorage},
+    ecs::prelude::{Join, ReadExpect, System, SystemData, World, Write, WriteStorage},
+    ui::UiText,
 };
 
-use crate::pong::{Ball, ARENA_WIDTH};
+use crate::pong::{Ball, ScoreBoard, ScoreText, ARENA_WIDTH};
 
 #[derive(SystemDesc)]
 pub struct WinnerSystem;
 
 impl<'s> System<'s> for WinnerSystem {
-    type SystemData = (WriteStorage<'s, Ball>, WriteStorage<'s, Transform>);
+    type SystemData = (
+        WriteStorage<'s, Ball>,
+        WriteStorage<'s, Transform>,
+        WriteStorage<'s, UiText>,
+        Write<'s, ScoreBoard>,
+        ReadExpect<'s, ScoreText>,
+    );
 
-    fn run(&mut self, (mut balls, mut locals): Self::SystemData) {
+    fn run(
+        &mut self,
+        (mut balls, mut locals, mut ui_text, mut scores, score_text): Self::SystemData,
+    ) {
         for (ball, transform) in (&mut balls, &mut locals).join() {
             let ball_x = transform.translation().x;
 
             let did_hit = if ball_x <= ball.radius {
-                println!("Player 2 Scores!");
+                scores.score_right = (scores.score_right + 1).min(999);
+                if let Some(text) = ui_text.get_mut(score_text.p2_score) {
+                    text.text = scores.score_right.to_string();
+                }
                 true
             } else if ball_x >= ARENA_WIDTH - ball.radius {
-                println!("Player 1 Scores!");
+                scores.score_left = (scores.score_left + 1).min(999);
+                if let Some(text) = ui_text.get_mut(score_text.p1_score) {
+                    text.text = scores.score_left.to_string();
+                }
                 true
             } else {
                 false
@@ -30,6 +46,10 @@ impl<'s> System<'s> for WinnerSystem {
             if did_hit {
                 ball.velocity[0] = -ball.velocity[0];
                 transform.set_translation_x(ARENA_WIDTH / 2.0);
+                println!(
+                    "Score: | {:^3} | {:^3} |",
+                    scores.score_left, scores.score_right
+                );
             }
         }
     }
