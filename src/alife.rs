25f7008c0efdfde333837b2ae96f3f135df8f520
zehreken <zehreken@gmail.com>
2019-12-09 01:01:28 +0100

    Switch to 3d rendering mode

---
+++ src/alife.rs
@@ -0,0 +1,10 @@
+use amethyst::prelude::*;
+
+#[derive(Default)]
+pub struct Alife {}
+
+impl SimpleState for Alife {
+    fn on_start(&mut self, data: StateData<'_, GameData<'_, '_>>) {
+
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,120 @@
+use amethyst::{
+    assets::{PrefabLoader, PrefabLoaderSystem, RonFormat},
+    core::transform::TransformBundle,
+    ecs::prelude::{ReadExpect, Resources, SystemData},
+    prelude::*,
+    renderer::{
+        pass::DrawShadedDesc,
+        rendy::{
+            factory::Factory,
+            graph::{
+                render::{RenderGroupDesc, SubpassBuilder},
+                GraphBuilder,
+            },
+            hal::{format::Format, image},
+            mesh::{Normal, Position, TexCoord},
+        },
+        types::DefaultBackend,
+        GraphCreator, RenderingSystem,
+    },
+    utils::{application_root_dir, scene::BasicScenePrefab},
+    window::{ScreenDimensions, Window, WindowBundle},
+};
+mod alife;
+use crate::alife::Alife;
+
+fn main() -> amethyst::Result<()> {
+    amethyst::start_logger(Default::default());
+
+    let app_root = application_root_dir()?;
+    let display_config_path = app_root.join("config").join("display.ron");
+
+    let game_data = GameDataBuilder::default()
+        .with_bundle(WindowBundle::from_config_path(display_config_path))?
+        .with_bundle(TransformBundle::new())?
+        .with_thread_local(RenderingSystem::<DefaultBackend, _>::new(
+            ExampleGraph::default(),
+        ));
+
+        let assets_dir = app_root.join("assets");
+    let mut game = Application::new(assets_dir, Alife::default(), game_data)?;
+    game.run();
+
+    Ok(())
+}
+
+#[derive(Default)]
+struct ExampleGraph {
+    dimensions: Option<ScreenDimensions>,
+    surface_format: Option<Format>,
+    dirty: bool,
+}
+
+impl GraphCreator<DefaultBackend> for ExampleGraph {
+    fn rebuild(&mut self, res: &Resources) -> bool {
+        // Rebuild when dimensions change, but wait until at least two frames have the same.
+        let new_dimensions = res.try_fetch::<ScreenDimensions>();
+        use std::ops::Deref;
+        if self.dimensions.as_ref() != new_dimensions.as_ref().map(|d| d.deref()) {
+            self.dirty = true;
+            self.dimensions = new_dimensions.map(|d| d.clone());
+            return false;
+        }
+        return self.dirty;
+    }
+
+    fn builder(
+        &mut self,
+        factory: &mut Factory<DefaultBackend>,
+        res: &Resources,
+    ) -> GraphBuilder<DefaultBackend, Resources> {
+        use amethyst::renderer::rendy::{
+            graph::present::PresentNode,
+            hal::command::{ClearDepthStencil, ClearValue},
+        };
+
+        self.dirty = false;
+        let window = <ReadExpect<'_, Window>>::fetch(res);
+        let surface = factory.create_surface(&window);
+        // cache surface format to speed things up
+        let surface_format = *self
+            .surface_format
+            .get_or_insert_with(|| factory.get_surface_format(&surface));
+        let dimensions = self.dimensions.as_ref().unwrap();
+        let window_kind =
+            image::Kind::D2(dimensions.width() as u32, dimensions.height() as u32, 1, 1);
+
+        let mut graph_builder = GraphBuilder::new();
+        let color = graph_builder.create_image(
+            window_kind,
+            1,
+            surface_format,
+            Some(ClearValue::Color([0.34, 0.36, 0.52, 1.0].into())),
+        );
+
+        let depth = graph_builder.create_image(
+            window_kind,
+            1,
+            Format::D32Sfloat,
+            Some(ClearValue::DepthStencil(ClearDepthStencil(1.0, 0))),
+        );
+
+        let opaque = graph_builder.add_node(
+            SubpassBuilder::new()
+                .with_group(DrawShadedDesc::new().builder())
+                .with_color(color)
+                .with_depth_stencil(depth)
+                .into_pass(),
+        );
+
+        let _present = graph_builder
+            .add_node(PresentNode::builder(factory, surface, color).with_dependency(opaque));
+
+        graph_builder
+    }
+}
+
+/*
 mod pong;
 mod systems;
 use crate::pong::Pong;
@@ -10,11 +127,12 @@ use amethyst::{
         plugins::{RenderFlat2D, RenderToWindow},
         types::DefaultBackend,
         RenderingBundle,
+        RenderingSystem,
     },
     utils::application_root_dir,
+    window::WindowBundle,
 };
-
-fn main() -> amethyst::Result<()> {
+fn main_() -> amethyst::Result<()> {
     // We'll put the rest of the code here.
     amethyst::start_logger(Default::default());
 
@@ -57,3 +175,4 @@ fn main() -> amethyst::Result<()> {
 
     Ok(())
 }
+*/
